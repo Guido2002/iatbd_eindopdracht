e1c2bacecbae599c134ccb8959f9840088065c3f
Guido2002 <guidoduif2002@gmail.com>
2022-04-07 13:52:55 +0200

    test

--- iatbd_eindopdracht/resources/views/components/header.blade.php
+++ iatbd_eindopdracht/resources/views/components/header.blade.php
@@ -1,16 +1,14 @@
 @if (auth()->user()->role == "admin")
 <header class="header">
+    nav
     <h1><a class="h1" href="/items">Time2Share</a></h1>
-    <h1><a class="h1" href="/logout">Logout</a></h1>
-    <h1><a class="h1" href="/items">Lenen</a></h1>
-    <h1><a class="h1" href="/block">Blokkeren</a></h1>
     <h1><a class="h1" href="/mijnprofiel">Mijn Profiel</a></h1>
+    <h1><a class="h1" href="/logout">Logout</a></h1>
 </header>
 @else
 <header class="header">
     <h1><a class="h1" href="/items">Time2Share</a></h1>
-    <h1><a class="h1" href="/logout">Logout</a></h1>
-    <h1><a class="h1" href="/items">Lenen</a></h1>
     <h1><a class="h1" href="/mijnprofiel">Mijn Profiel</a></h1>
+    <h1><a class="h1" href="/logout">Logout</a></h1>
 </header>
 @endif
